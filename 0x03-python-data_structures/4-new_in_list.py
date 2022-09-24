e2de7f2f31fe1f3528f8cf0ba632796cf3bfd5f2
Clintonnyakundi <mokayaclinton34@gmail.com>
2022-09-24 03:39:32 -0700

    Fix code to return original list when conditions not met

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,4 +3,6 @@ def new_in_list(my_list, idx, element):
     if 0 <= idx < len(my_list):
         copyList = my_list.copy()
         copyList[idx] = element
-    return (copyList)
+        return (copyList)
+    else:
+        return (my_list)
