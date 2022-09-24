f42471fb35a47049fc00a04e415efe3cde09ed63
Clintonnyakundi <mokayaclinton34@gmail.com>
2022-09-24 03:35:28 -0700

    Write function to replace element at an index of a list without modifying original list

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    if 0 <= idx < len(my_list):
+        copyList = my_list.copy()
+        copyList[idx] = element
+    return (copyList)
