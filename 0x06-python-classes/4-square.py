c863fd271c74ed3e7a27c38d6616886c427bc2b9
Clintonnyakundi <mokayaclinton34@gmail.com>
2022-10-12 13:47:49 -0700

    Create getters and setters for the class

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Make a class square."""
+
+
+class Square:
+    """Represent the square"""
+
+    def __init__(self, size=0):
+        """Initialize the new square.
+        Args:
+            size (int): Size of the new square.
+        """
+        self.__size = size
+    
+    @property
+    def size(self):
+        """Set current size of square"""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Returns the current square area"""
+        return (self.__size ** 2)
