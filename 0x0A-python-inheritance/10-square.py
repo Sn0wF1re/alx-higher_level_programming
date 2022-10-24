0e5e32441939ca6c4c011f82357726c4c5811097
Clintonnyakundi <mokayaclinton34@gmail.com>
2022-10-24 14:57:06 -0700

    Write a class 'Square' that inherits from 'Rectangle'

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""creates class that inherits Rectangle class"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Defines a square"""
+    def __init__(self, size):
+        """instantiates square size"""
+        super().integer_validator('size', size)
+        super().__init__(size, size)
+        self.__size = size
