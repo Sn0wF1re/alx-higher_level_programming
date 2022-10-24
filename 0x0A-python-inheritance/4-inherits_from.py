ceb9b45349eaba2454428e92c2ac5cc2cf83af20
Clintonnyakundi <mokayaclinton34@gmail.com>
2022-10-24 12:59:30 -0700

    Write function to check for an instance of a subclass

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,18 @@
+#!/usr/bini/python3
+"""Creates function inherits_from"""
+
+
+def inherits_from(obj, a_class):
+    """Checks whether object is an instance
+    of an inherited class
+
+    Args:
+        obj: object to be checked
+        a_class: class to be checked against
+
+    Return: True if object is an instance of a_class
+            False otherwise
+    """
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
+        return (True)
+    return (False)
