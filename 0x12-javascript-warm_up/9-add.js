b387456ecaa79650d73db0e2ee22b3e1e9595898
Clintonnyakundi <mokayaclinton34@gmail.com>
2022-12-19 05:55:31 -0800

    Fix code for script to add two integers

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-// prints the addition of 2 integers
-function add(a, b) {
-  return (a + b);
+function add (a, b) {
+  return a + b;
 }
+
 console.log(add(Number(process.argv[2]), Number(process.argv[3])));
