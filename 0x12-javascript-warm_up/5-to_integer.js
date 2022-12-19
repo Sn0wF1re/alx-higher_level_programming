be57b3fb8728dc547230e08f673aa6a4b1423c1f
Clintonnyakundi <mokayaclinton34@gmail.com>
2022-12-19 04:43:41 -0800

    Write a script that prints My number: <first argument converted in integer>

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+/* prints My number: <first argument converted in integer>
+if the first argument can be converted to an integer
+*/
+const num = Math.floor(Number(process.argv[2]));
+if (isNaN(num)) {
+  console.log('Not a number');
+} else {
+  console.log(`My number: ${num}`);
+}
