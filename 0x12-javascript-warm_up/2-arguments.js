b59c9c2b719214be082bd1881d4c170f5d1ad481
Clintonnyakundi <mokayaclinton34@gmail.com>
2022-12-19 03:46:21 -0800

    Fix code to match standard rules

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
 /* prints a message depending of the number of arguments passed */
 argLength = process.argv.length;
-if (argLength == 2) {
+if (argLength === 2) {
   console.log('No argument');
-}
-else if (argLength == 3){
+} else if (argLength === 3) {
   console.log('Argument found');
-}else {
+} else {
   console.log('Arguments found');
 }
--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,4 +1,3 @@
 #!/usr/bin/node
 /* prints the first argument passed to it */
-const argLength = process.argv.length;
-console.log(argLength === 2 ? 'No argument' : process.argv[2]); 
+console.log(process.argv[2] === undefined ? 'No argument' : process.argv[2]); 
