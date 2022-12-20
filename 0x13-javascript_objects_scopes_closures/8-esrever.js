4ee2ea66e4a6211b6c74af72578f4a51271c9fa6
Clintonnyakundi <mokayaclinton34@gmail.com>
2022-12-20 05:29:10 -0800

    Fix spacing in script

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
 exports.esrever = function (list) {
-  let myArray = [];
+  let myList = [];
   for (let i = list.length - 1; i >= 0; i--) {
-    myArray.push(list[i]);
+    myList.push(list[i]);
   }
-  return myArray;
+  return myList;
 };
