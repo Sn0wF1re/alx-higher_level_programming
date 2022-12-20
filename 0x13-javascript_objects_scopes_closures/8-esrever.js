1c1064daf60ac6303cc240200fa691b26f1ce944
Clintonnyakundi <mokayaclinton34@gmail.com>
2022-12-20 05:39:25 -0800

    Fix script to pass requirements

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,9 +1,5 @@
 #!/usr/bin/node
 
 exports.esrever = function (list) {
-  let myList = [];
-  for (let i = list.length - 1; i >= 0; i--) {
-    myList.push(list[i]);
-  }
-  return myList;
+  return list.sort((a, b) => b - a);
 };
