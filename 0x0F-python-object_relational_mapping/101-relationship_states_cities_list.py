a35fce7a2b78d49b36404de39b6e5e854d2ec2e7
Clintonnyakundi <mokayaclinton34@gmail.com>
2022-12-22 14:56:37 -0800

    Finish up polishes

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -17,6 +17,6 @@ if __name__ == "__main__":
     session = Session()
 
     for state in session.query(State).order_by(State.id):
-        print("{}: {}".format(state.id, state.name))
+        print(f"{state.id}: {state.name}")
         for city in state.cities:
-            print("    {}: {}".format(city.id, city.name))
+            print(f"    {city.id}: {city.name}")
