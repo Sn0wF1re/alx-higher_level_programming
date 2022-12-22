73ba2b6ed7950557acd570905ff7efd86547d461
Clintonnyakundi <mokayaclinton34@gmail.com>
2022-12-22 08:31:32 -0800

    Remove trailing whitespace

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -16,5 +16,5 @@ if __name__ == "__main__":
     session = Session()
 
     for instance in session.query(State).filter(State.name.like('%a%')):
-        session.delete(instance) 
+        session.delete(instance)
     session.commit()
