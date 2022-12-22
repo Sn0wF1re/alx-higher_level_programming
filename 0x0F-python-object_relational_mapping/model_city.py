142a89719d137e08cfc377072ec5d99ae2e168d0
Clintonnyakundi <mokayaclinton34@gmail.com>
2022-12-22 08:24:50 -0800

    Create City class and print all City objects from the database hbtn_0e_14_usa

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
 """
-defines a class State
+defines a class City
 """
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
 
-class State(Base):
+class City(Base):
     """
     inherits from Base
-    links to the MySQL table states
+    links to the MySQL table cities
     """
-    __tablename__ = "states"
+    __tablename__ = "cities"
     id = Column(Integer, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
