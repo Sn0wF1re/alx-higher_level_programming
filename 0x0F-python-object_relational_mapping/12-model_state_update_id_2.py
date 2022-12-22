07e35bd985e89c53a4cef3998411f4bc3b68e2e1
Clintonnyakundi <mokayaclinton34@gmail.com>
2022-12-22 07:32:14 -0800

    Fix script to match the first instance

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -14,6 +14,6 @@ if __name__ == '__main__':
                            pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
-    updateState = session.query(State).filter_by(id=2)
+    updateState = session.query(State).filter_by(id=2).first()
     updateState.name = "New Mexico"
     session.commit()
