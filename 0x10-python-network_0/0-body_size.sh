285a48beb9893995d3801c8555be842bc369129d
Clintonnyakundi <mokayaclinton34@gmail.com>
2023-01-16 02:15:45 -0800

    Make script 3 lines long

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# takes in a URL, sends a request to that URL,
-# and displays the size of the body of the response
-
+# takes in a URL, sends request to that URL, displays size of body of response
 curl -sI "$1" | grep -i Content-Length | cut -d ' ' -f 2 
