fe180c36b4c706620378591f872740287152804a
Clintonnyakundi <mokayaclinton34@gmail.com>
2023-01-17 09:40:51 -0800

    Write script to return response or raise errors with status above 400

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+takes in a URL, sends a request to the URL and displays the body
+of the response
+If the HTTP status code is greater than or equal to 400,
+print: Error code: followed by the value of the HTTP status code
+You must use the packages requests and sys
+"""
+
+
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+
+    r = requests.get(argv[1])
+    if r.status_code >= 400:
+        print("Error code:", r.status_code)
+    else:
+        print(r.text)
