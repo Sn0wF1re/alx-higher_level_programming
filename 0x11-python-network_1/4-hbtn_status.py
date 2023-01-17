2e1d1f795841b4ec82ad78d298b4287811c71336
Clintonnyakundi <mokayaclinton34@gmail.com>
2023-01-17 10:03:49 -0800

    Fix script

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 """
-Write a Python script that fetches https://alx-intranet.hbtn.io/status
-using requests package
+Python script that fetches an URL with requests package
 """
+import requests
 
 
 if __name__ == "__main__":
-    import requests
-
-    r = requests.get("https://alx-intranet.hbtn.io/status")
-    print("Body response:")
-    print(f"\t- type: {type(r.text)}")
-    print(f"\t- content: {r.text}")
+    r = requests.get('https://alx-intranet.hbtn.io/status')
+    t = r.text
+    print('Body response:\n\t- type: {}\n\t- content: {}'.format(type(t), t))
