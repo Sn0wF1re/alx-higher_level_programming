1edc014acffcd8c81be3ec6f1ba2afadfb09b4b0
Clintonnyakundi <mokayaclinton34@gmail.com>
2023-01-17 07:59:16 -0800

    Fix script to use 'with' statement

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -12,7 +12,8 @@ if __name__ == "__main__":
 
     req = Request(argv[1])
     try:
-        response = urlopen(req)
+        with urlopen(req) as response:
+            print(response.read().decode('utf-8'))
     except URLError as e:
         if hasattr(e, 'code'):
             print("Error code: ", e.code)
