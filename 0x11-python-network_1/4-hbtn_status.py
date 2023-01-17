b54e94d11435b5a8e401d293791ea19d38b72615
Clintonnyakundi <mokayaclinton34@gmail.com>
2023-01-17 08:25:05 -0800

    Fix script to show correct type of response

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -10,5 +10,5 @@ if __name__ == "__main__":
 
     r = requests.get("https://alx-intranet.hbtn.io/status")
     print("Body response:")
-    print(f"\t- type: {type(r)}")
+    print(f"\t- type: {type(r.text)}")
     print(f"\t- content: {r.text}")
