23ee2e6ae6e126fb00bef75477572bc92d10f7c2
Clintonnyakundi <mokayaclinton34@gmail.com>
2023-01-17 13:29:43 -0800

    Fix script

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -4,6 +4,9 @@ Write a Python script that takes 2 arguments in order to solve this challenge
 """
 
 
+import requests
+from sys import argv
+
 if __name__ == '__main__':
     url = "https://api.github.com/repos/{}/{}commits"\
           .format(argv[2], argv[1])
