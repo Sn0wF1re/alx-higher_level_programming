bf39ad9c808fae56f8c355b8495cae6e9022e528
Clintonnyakundi <mokayaclinton34@gmail.com>
2023-01-17 13:23:11 -0800

    Fix script

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -12,7 +12,5 @@ if __name__ == "__main__":
     r = requests.get(url)
     commits = r.json()
     for i in range(10):
-        print("{}: {}".format(
-            commits[i].get('sha'),
-            commits[i].get('commit').get('author').get('name')
-            ))
+        print(commits[i].get('sha'), end=": ")
+        print(commits[i].get('commit').get('author').get('name'))
