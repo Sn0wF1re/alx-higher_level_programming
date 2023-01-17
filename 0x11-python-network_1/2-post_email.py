dbaa268c8abf485108b2aef12e1d8fc426860366
Clintonnyakundi <mokayaclinton34@gmail.com>
2023-01-17 12:24:07 -0800

    Fix script to pass all tests

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -6,13 +6,16 @@ displays the body of the response (decoded in utf-8)
 """
 
 
-import urllib.request
-import urllib.parse
 import sys
+import urllib.parse
+import urllib.request
 
-if __name__ == '__main__':
-    data = urllib.parse.urlencode({'email': sys.argv[2]}).encode('ascii')
-    req = urllib.request.Request(sys.argv[1], data)
-    with urllib.request.urlopen(req) as r:
-        html = r.read()
-        print(f"{html.decode('utf-8')}")
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    value = {"email": sys.argv[2]}
+    data = urllib.parse.urlencode(value).encode("ascii")
+
+    request = urllib.request.Request(url, data)
+    with urllib.request.urlopen(request) as response:
+        print(response.read().decode("utf-8"))
