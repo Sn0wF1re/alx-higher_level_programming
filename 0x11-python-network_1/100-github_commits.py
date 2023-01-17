c4e1de2801731c39f0118468216eb1914f3ba444
Clintonnyakundi <mokayaclinton34@gmail.com>
2023-01-17 13:26:10 -0800

    Fix script

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -4,13 +4,12 @@ Write a Python script that takes 2 arguments in order to solve this challenge
 """
 
 
-if __name__ == "__main__":
-    import requests
-    from sys import argv
-
-    url = f"https://api.github.com/repos/{argv[2]}/{argv[1]}/commits"
+if __name__ == '__main__':
+    url = "https://api.github.com/repos/{}/{}commits"\
+          .format(argv[2], argv[1])
     r = requests.get(url)
     commits = r.json()
-    for i in range(10):
-        print(commits[i].get('sha'), end=": ")
-        print(commits[i].get('commit').get('author').get('name'))
+
+    for commit in commits[:10]:
+        print(commit.get('sha'), end=': ')
+        print(commit.get('commit').get('author').get('name'))
