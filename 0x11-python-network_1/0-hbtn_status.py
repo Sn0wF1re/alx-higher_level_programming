a0240cd15a8518880dfa3bdd939fe024abeccb3a
Clintonnyakundi <mokayaclinton34@gmail.com>
2023-01-17 06:48:19 -0800

    Write a Python script that fetches https://alx-intranet.hbtn.io/status

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Write a Python script that fetches https://alx-intranet.hbtn.io/status
+"""
+
+
+if __name__ == "__main__":
+    import urllib.request as request
+
+    with request.urlopen("https://alx-intranet.hbtn.io/status") as response:
+        html = response.read()
+        print("Body response:")
+        print(f"\t- type: {type(html)}")
+        print(f"\t- content: {html}")
+        print(f"\t- utf8 content: {html.decode('utf-8')}")
