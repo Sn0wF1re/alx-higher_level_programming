ed5175f30e661caddaea7dfe8e6926bf4187fb46
Clintonnyakundi <mokayaclinton34@gmail.com>
2023-02-09 05:42:12 -0800

    Change from double quotes to single-quoted strings

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -4,9 +4,6 @@ const request = require('request');
 const url = process.argv[2];
 
 request(url, (error, response) => {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log("code: " + response.statusCode);
-  }
+  if (error) console.log(error);
+  console.log('code: ' + response.statusCode);
 });
